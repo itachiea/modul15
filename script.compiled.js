d80aa480d06b99640783948f904da403c7a5687e
ichi <w.konrad60@gmail.com>
2017-11-12 18:18:45 +0100

    jasne

--- script.compiled.js
+++ script.compiled.js
@@ -1,57 +1,28 @@
-'use strict';
+"use strict";
 
-var sayHello = function sayHello() {
-	return alert('Hello world');
-};
-sayHello();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-//----------Zadania submoduł 15.4
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-//--- Zadanie 1
+var Point = function () {
+    function Point(x, y) {
+        _classCallCheck(this, Point);
 
-var string1 = 'mleko',
-    string2 = 'jajko';
+        this.x = x;
+        this.y = y;
+    }
 
-var strings = string1 + ', ' + string2; //interpolacja :)
+    _createClass(Point, [{
+        key: "toString",
+        value: function toString() {
+            return "(" + this.x + ", " + this.y + ")";
+        }
+    }], [{
+        key: "print",
+        value: function print(text) {
+            console.log(text);
+        }
+    }]);
 
-//--- Zadanie 2
-
-var x = 2,
-    y = 5;
-
-var multiply = function multiply() {
-	var z = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 1;
-	var c = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 1;
-	return console.log(z * c);
-}; // (z = 1, c = 1) - domyślne parametry :)
-multiply(x, y);
-
-//--- Zadanie 3
-
-var average = function average() {
-	for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-		args[_key] = arguments[_key];
-	}
-
-	console.log(args);
-	var i = 0;
-	args.forEach(function (arg) {
-		return i += arg;
-	});
-	return console.log(i / args.length);
-};
-
-//average(1, 7, 9, 11); // <----- do zadania 3 :)
-
-//---Zadanie 4
-
-var grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-average.apply(undefined, grades);
-
-//--- Zadanie 5
-
-var dane = [1, 4, 'Iwona', false, 'Nowak'];
-var firstName = dane[2],
-    lastName = dane[4];
-
-console.log(firstName + ' ' + lastName);
+    return Point;
+}();
--- script.js
+++ script.js
@@ -1,42 +1,12 @@
-const sayHello = () => alert('Hello world');
-sayHello();
-
-//----------Zadania submoduł 15.4
-
-//--- Zadanie 1
-
-const string1 = 'mleko',
-	string2 = 'jajko';
-
-const strings = `${string1}, ${string2}`; //interpolacja :)
-
-//--- Zadanie 2
-
-const x = 2,
-	y = 5;
-
-const multiply = (z = 1, c = 1) => console.log(z * c); // (z = 1, c = 1) - domyślne parametry :)
-multiply(x, y); 
-
-//--- Zadanie 3
-
-const average = (...args) => {
-	console.log(args);
-	let i = 0;
-		args.forEach(arg => i += arg);
-	return console.log(i / args.length);
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    toString() {
+        return `(${this.x}, ${this.y})`;
+    }
+    static print(text) {
+        console.log(text);
+    }
 }
-
-//average(1, 7, 9, 11); // <----- do zadania 3 :)
-
-//---Zadanie 4
-
-const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-average(...grades);
-
-
-//--- Zadanie 5
-
-const dane = [1, 4, 'Iwona', false, 'Nowak'];
-const [ , , firstName, , lastName] = dane;
-console.log(firstName + ' ' + lastName);
